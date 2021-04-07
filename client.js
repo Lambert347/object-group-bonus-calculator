436f417c1b4a54b5fcb656a8597d4431430e09c5
Ian Lambert <lambe347@umn.edu>
2021-04-07 10:34:54 -0500

    looks good, needs comments, stretch goals need to at least be attempted

--- client.js
+++ client.js
@@ -42,11 +42,12 @@ const employees = [
 
 console.log( employees );
 
+
 const newEmployees = [];
 
-function makeEmployees (name, bonusPercentage, totalCompensation, totalBonus){
+function updateEmployees (name, bonusPercentage, totalCompensation, totalBonus ) {
   let employee = {
-    name: name,
+    name,
     bonusPercentage,
     totalCompensation,
     totalBonus,
@@ -54,33 +55,66 @@ function makeEmployees (name, bonusPercentage, totalCompensation, totalBonus){
   newEmployees.push(employee);
   return newEmployees;
 }
-makeEmployees('Atticus', 0, 0, 0);
-makeEmployees('Jem', 0, 0, 0);
-makeEmployees('Scout', 0, 0, 0);
-makeEmployees('Robert', 0, 0, 0);
-makeEmployees('Mayella', 0, 0, 0);
+updateEmployees('Atticus', 0, 0, 0);
+updateEmployees('Jem', 0, 0, 0);
+updateEmployees('Scout', 0, 0, 0);
+updateEmployees('Robert', 0, 0, 0);
+updateEmployees('Mayella', 0, 0, 0);
 console.log(newEmployees);
+
+
 
 function updateBonus (array, array2) {
   for (let i = 0, j = 0; i < array.length, j < array2.length; i++, j++) {
     if (array2[j].reviewRating <= 2) {
       array[i].bonusPercentage = 0;
     }
-    else if (array2[j].reviewRating === 3){
+    else if(array2[j].reviewRating === 3){
       array[i].bonusPercentage = 4;
     }
-    else if (array2[j].reviewRating === 4){
+    else if(array2[j].reviewRating === 4){
       array[i].bonusPercentage = 6;
     }
     else if(array2[j].reviewRating === 5){
       array[i].bonusPercentage = 10;
     }
   }
-
+  for (let x = 0, y = 0; x < array.length, y < array2.length; x++, y++) {
+    parseInt(array2[y].annualSalary, 10);
+    if (array2[y].annualSalary > 65000){
+      array[x].bonusPercentage -= 1;
+    }
+    if (array2[y].employeeNumber.length === 4){
+      array[x].bonusPercentage += 5;
+    }
+  }
+  for (let o = 0, v = 0; o < array.length, v < array2.length; o++, v++){
+    if (array[o].bonusPercentage > 13){
+      array[o].bonusPercentage = 13;
+    }
+    else if (array[o].bonusPercentage < 0){
+      array[o].bonusPercentage = 0;
+    }
+  }
   return array;
 }
 console.log(updateBonus(newEmployees, employees));
 console.log(newEmployees);
+
+function calculateBonus (array, array2){
+  for (let i = 0, j = 0; i < array.length, j < array2.length; i++, j++){
+    array2[j].annualSalary = parseInt(array2[j].annualSalary, 10);
+    array[i].bonusPercentage = array[i].bonusPercentage / 100;
+    array[i].totalBonus = array2[j].annualSalary * array[i].bonusPercentage;
+    array[i].totalBonus = Math.round(array[i].totalBonus);
+    array[i].totalCompensation = array[i].totalBonus + array2[j].annualSalary;
+  }
+return array;
+}
+
+calculateBonus(newEmployees, employees);
+console.log(newEmployees);
+
 
 // function addBonus(array){
 //   let bonusPercentage = 0;
